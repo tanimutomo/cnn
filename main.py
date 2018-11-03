45531ed130d9d21faf3fb285737643dfa543dca7
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2018-11-03 15:38:10 +0900

    commit violently

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def main():
     test_loader = DataLoader(test_data, batch_size=4, shuffle=False)
 
     trainer = Trainer(train_loader, test_loader, CNN(), device)
-    trainer.train(epochs=3, lr=1e-3)
+    trainer.train(epochs=5, lr=1e-3)
 
 if __name__ == '__main__':
     print('BEGIN CNN MAIN')
